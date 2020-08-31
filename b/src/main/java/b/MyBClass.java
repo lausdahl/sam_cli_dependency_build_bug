7bccbeb9b313fab10158eb53b749dfee8d90edd5
Kenneth Lausdahl <kgl@mjolner.dk>
2020-08-31 13:18:47 +0200

    initial commit

---
+++ b/src/main/java/b/MyBClass.java
@@ -0,0 +1,7 @@
+package b;
+
+public class MyBClass {
+    public String getName(){
+        return this.getClass().getName();
+    }
+}
---
+++ c/src/main/java/c/MyCClass.java
@@ -0,0 +1,6 @@
+package c;
+public class MyCClass {
+    public String getName(){
+        return this.getClass().getName();
+    }
+}
